cece0f46a4716a6802c8bf3ab027721e812fd7ab
Saurabh Kamboj <skamboj97@gmail.com>
2023-05-12 14:45:49 +0530

    Modify file

--- object_oriented_programming/easy1/banner_class.rb
+++ object_oriented_programming/easy1/banner_class.rb
@@ -28,20 +28,37 @@ class Banner
   end
 end
 
+# Further exploration
+
+class Banner
+  def initialize(message, width)
+    @message = message
+    @width = width > message.size ? width : message.size
+  end
+
+  def to_s
+    [horizontal_rule, empty_line, message_line, empty_line, horizontal_rule].join("\n")
+  end
+
+  private
+
+  def horizontal_rule
+    "+-#{'-' * @width}-+"
+  end
+
+  def empty_line
+    "| #{' ' * @width} |"
+  end
+
+  def message_line
+    "| #{@message.center(@width)} |"
+  end
+end
+
 # Test Cases
 
-banner = Banner.new('To boldly go where no one has gone before.')
+banner = Banner.new('To boldly go where no one has gone before.', 10)
 puts banner
-# +--------------------------------------------+
-# |                                            |
-# | To boldly go where no one has gone before. |
-# |                                            |
-# +--------------------------------------------+
 
-banner = Banner.new('')
+banner = Banner.new('', 5)
 puts banner
-# +--+
-# |  |
-# |  |
-# |  |
-# +--+
