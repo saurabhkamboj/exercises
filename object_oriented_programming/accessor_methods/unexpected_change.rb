07ddc710b847d44d9d496a07021b1a5a039d599a
Saurabh Kamboj <skamboj97@gmail.com>
2023-05-12 13:37:28 +0530

    Add file

---
+++ object_oriented_programming/accessor_methods/unexpected_change.rb
@@ -0,0 +1,33 @@
+# Modify the following code to accept a string containing a first and last name. The name should be split into two instance variables in the setter method, then joined in the getter method to form a full name.
+
+# Expected output:
+
+# John Doe
+
+class Person
+  def name=(full_name)
+    parts = full_name.split
+    @first_name = parts.first
+    @last_name = parts.last
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
+  end
+end
+
+person1 = Person.new
+person1.name = 'John Doe'
+puts person1.name
+
+# Alternate by Launch School
+
+class Person
+  def name=(full_name)
+    first_name, last_name = full_name.split
+  end
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+end
