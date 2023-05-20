348a13805dd25efe4189d0e69c1fbd374092b795
Saurabh Kamboj <skamboj97@gmail.com>
2023-05-20 18:26:04 +0530

    Add file

---
+++ object_oriented_programming/easy1/rectangles_and_squares.rb
@@ -0,0 +1,23 @@
+# Given the following class:
+
+class Rectangle
+  def initialize(height, width)
+    @height = height
+    @width = width
+  end
+
+  def area
+    @height * @width
+  end
+end
+
+# Write a class called Square that inherits from Rectangle, and is used like this:
+
+class Square < Rectangle
+  def initialize(side)
+    super(side, side)
+  end
+end
+
+square = Square.new(5)
+puts "area of square = #{square.area}"
