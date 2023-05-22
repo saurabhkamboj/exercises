36a27e23a42305f234b58285712ba66499d4180b
Saurabh Kamboj <skamboj97@gmail.com>
2023-05-22 13:51:06 +0530

    Add file

---
+++ object_oriented_programming/easy2/pet_shelter.rb
@@ -0,0 +1,90 @@
+# Consider the following code:
+
+# Write the classes and methods that will be necessary to make this code run, and print the following output:
+
+# P Hanson has adopted the following pets:
+# a cat named Butterscotch
+# a cat named Pudding
+# a bearded dragon named Darwin
+
+# B Holmes has adopted the following pets:
+# a dog named Molly
+# a parakeet named Sweetie Pie
+# a dog named Kennedy
+# a fish named Chester
+
+# P Hanson has 3 adopted pets.
+# B Holmes has 4 adopted pets.
+
+# The order of the output does not matter, so long as all of the information is presented.
+
+class Pet
+  attr_reader :type, :name
+
+  def initialize(type, name)
+    @type = type
+    @name = name
+  end
+end
+
+class Owner
+  attr_reader :name
+  attr_accessor :pets
+
+  def initialize(name)
+    @name = name
+    @pets = []
+  end
+
+  def number_of_pets
+    @pets.size
+  end
+end
+
+class Shelter
+  def initialize
+    @adoptions = {}
+  end
+
+  def adopt(owner, pet)
+    owner.pets << pet
+
+    if @adoptions.key?(owner)
+      @adoptions[owner] << pet
+    else
+      @adoptions[owner] = [pet]
+    end
+  end
+
+  def print_adoptions
+    @adoptions.each do |owner, pets|
+      puts "#{owner.name} has adopted the following pets:"
+      pets.each do |pet|
+        puts "a #{pet.type} named #{pet.name}"
+      end
+    end
+  end
+end
+
+butterscotch = Pet.new('cat', 'Butterscotch')
+pudding      = Pet.new('cat', 'Pudding')
+darwin       = Pet.new('bearded dragon', 'Darwin')
+kennedy      = Pet.new('dog', 'Kennedy')
+sweetie      = Pet.new('parakeet', 'Sweetie Pie')
+molly        = Pet.new('dog', 'Molly')
+chester      = Pet.new('fish', 'Chester')
+
+phanson = Owner.new('P Hanson')
+bholmes = Owner.new('B Holmes')
+
+shelter = Shelter.new
+shelter.adopt(phanson, butterscotch)
+shelter.adopt(phanson, pudding)
+shelter.adopt(phanson, darwin)
+shelter.adopt(bholmes, kennedy)
+shelter.adopt(bholmes, sweetie)
+shelter.adopt(bholmes, molly)
+shelter.adopt(bholmes, chester)
+shelter.print_adoptions
+puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
+puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
