db0b322706ca181a7776e638f476eba3e0d39de4
Saurabh Kamboj <skamboj97@gmail.com>
2023-11-27 18:01:03 +0530

    Add file

---
+++ ruby_foundations_more_topics/easy2/each_with_object.rb
@@ -0,0 +1,34 @@
+def each_with_object(array, accumulator_object)
+  array.each { |element| yield(element, accumulator_object) }
+  accumulator_object
+end
+
+# Alternate
+
+def each_with_object(array, accumulator_object)
+  for element in array
+    yield(element, accumulator_object)
+  end
+
+  accumulator_object
+end
+
+result = each_with_object([1, 3, 5], []) do |value, list|
+  list << value**2
+end
+p result == [1, 9, 25]
+
+result = each_with_object([1, 3, 5], []) do |value, list|
+  list << (1..value).to_a
+end
+p result == [[1], [1, 2, 3], [1, 2, 3, 4, 5]]
+
+result = each_with_object([1, 3, 5], {}) do |value, hash|
+  hash[value] = value**2
+end
+p result == { 1 => 1, 3 => 9, 5 => 25 }
+
+result = each_with_object([], {}) do |value, hash|
+  hash[value] = value * 2
+end
+p result == {}
