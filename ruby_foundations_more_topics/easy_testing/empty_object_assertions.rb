b728365b3d7605ee652637acc8698f463a0ffcfc
Saurabh Kamboj <skamboj97@gmail.com>
2023-11-28 14:45:42 +0530

    Add file

---
+++ ruby_foundations_more_topics/easy_testing/empty_object_assertions.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+
+class EmptyObjectAssertion < Minitest::Test
+  def test_empty_object
+    list = []
+    assert_empty list, "The list is not empty!"
+  end
+end
+
+# Alternate
+assert_equal true, list.empty?
