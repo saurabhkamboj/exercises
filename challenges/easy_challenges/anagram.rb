0787443ea84b1138966994a3fa2ef4c322463f79
Saurabh Kamboj <skamboj97@gmail.com>
2024-01-03 23:41:31 +0530

    Add and modify files

---
+++ challenges/easy_challenges/anagram.rb
@@ -0,0 +1,69 @@
+# Problem => https://launchschool.com/exercises/b7be1b38
+
+=begin
+  Details
+    - Detect anagrams of a given word.
+    - Anagram is a word or a phrase formed by re-arranging the letters of a different word or phrase.
+    - The detection is case in-sensitive.
+    - Identical words are no anagrams.
+
+  Examples
+    - An `Anagram` class has to be created. The class should have 2 methods:
+      - A constructor that takes an integer as an argument. The constructor does not raise any errors.
+      - A method that takes an array of strings as argument and returns an array containing anagrams. The name of the method will be `#match`.
+
+  Data structures
+    - Input => The input for the constructor method is a string and for `#match` instance method it's an array.
+    - Output => Array of anagrams
+
+  Algortihm
+    Constructor
+    - Take a string an argument.
+    - Assign it in lowercase to an instance variable.
+
+    `#match` method
+    - Take an array as an argument.
+    - Iterate over each element of the array. On each iteration:
+      - Skip iteration is element.downcase == input.downcase
+      - Compare the sorted lower case characters of the current element with sorted characters of input string.
+      - Select the element if the comparison returns true.
+    - Return the array of selected elements.
+=end
+
+class Anagram
+  def initialize(string)
+    @input = string.downcase
+  end
+
+  def match(array)
+    array.select do |element|
+      next if element.downcase == @input
+      element.downcase.chars.sort == @input.chars.sort
+    end
+  end
+end
+
+# Alternate
+class Anagram
+  attr_reader :input
+
+  def initialize(word)
+    @input = word.downcase
+  end
+
+  def match(word_array)
+    word_array.select do |element|
+      element.downcase != input && anagram?(element, input)
+    end
+  end
+
+  private
+
+  def sorted_chars(word)
+    word.downcase.chars.sort
+  end
+
+  def anagram?(word1, word2)
+    sorted_chars(word1) == sorted_chars(word2)
+  end
+end
--- challenges/easy_challenges/roman_numerals.rb
+++ challenges/easy_challenges/roman_numerals.rb
@@ -2,23 +2,47 @@
 
 =begin
   Breakdown
-    - Convert decimals into roman numbers.
-    - Roman letters => I, V, X, L, C, D, M
-      - I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
-      - A letter can only be repeated thrice.
-      - Therefore, IV = 4 (5 - 1)
-      - Letter to the left when substracting
-      - Letter to the right when adding
-    - Input => Integer
-    - Output => String
+    - For our program, the details we need to keep in mind are as follows:
+      - I stands for 1
+      - V stands for 5
+      - X stands for 10
+      - L stands for 50
+      - C stands for 100
+      - D stands for 500
+      - M stands for 1,000
+    - We don't have to worry about numbers higher than 3,000.
+    - Modern Roman numerals are written by expressing each digit of the number separately, starting with the left most digit and skipping any digit with a value of zero.
 
-  Data structure
-    - 27, find all tens, hundreds and thousands
-    - 27.digits.map.with_index { (_1.to_s + '0' * _2).to_i }.reverse
-    - [20, 7], [1, 5, 10, 50, 100, 500, 1000]
+  Examples
+    - From the test cases shown above, we can see that we need to create a RomanNumeral class. The class should have 2 methods as follows:
+      - A constructor that accepts a non-Roman integer number as an argument. From the test cases, it appears that the constructor does not raise any errors.
+      - A method that returns the Roman numeral representation of that number as a string. The name of this method will be either to_roman or toRoman depending on the conventions for your programming language of choice.
+    - We may also want to create some helper methods to assist us, but those are optional.
+
+  Data strctures
+    - Our input is an integer in our language of choice, and our output is a string. We may want to use some type of collection to help us create the desired output since it can make iteration easier. It might also be a good idea to have another collection hold some key conversions between numbers and Roman numerals.
+
+    - We may also need a separate collection that holds key conversions between numbers and Roman numerals. How would ascending or descending order impact its ease of use?
+
+  Algorithm
+    Constructor
+    - Accept a number as an argument.
+    - Save it for later use.
+
+    Roman Numerals Collection
+    - Create a collection that links important Roman numeral strings to their numeric counterparts.
+    - Ensure this is in descending order.
+
+    Method: Convert to Roman Numerals (Ruby: to_roman; JavaScript: toRoman)
+    - Initialize a variable with an empty string to save the finished Roman conversion.
+    - Iterate over the Roman Numerals collection:
+      - If the numeric value of the current Roman numeral is less than the value of the input number, add the Roman numerals to the string as many times as its value can fit. For instance, if the current Roman numeral is C (which has a value of 100) and the input number is 367, then 3 C's are needed: CCC.
+      - Subtract the numeric value of the added Roman numerals from the current input value, and use the new input value in subsequent iterations. For instance, since we added CCC to the string above, we must subtract 300 from 367, leaving us with a new input number of 67.
+
+    Return the result string.
 =end
 
-# Solution by Patrik Moran
+# Solution by Patrick Moran
 class RomanNumeral
   def initialize(number)
     self.number = number
