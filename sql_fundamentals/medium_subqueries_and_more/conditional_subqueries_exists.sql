c955947c5fd866c91e359219ce2ee58c14b4555c
Saurabh Kamboj <skamboj97@gmail.com>
2024-10-12 09:22:09 +0530

    Modify file

---
+++ sql_fundamentals/medium_subqueries_and_more/conditional_subqueries_exists.sql
@@ -0,0 +1,19 @@
+SELECT name FROM bidders WHERE EXISTS
+  (SELECT bidder_id FROM bids
+    WHERE bidder_id = bidders.id);
+
+-- Further exploration
+
+SELECT DISTINCT bidders.name
+  FROM bidders
+  JOIN bids
+    ON bidders.id = bids.bidder_id;
+
+-- Alternate
+
+SELECT bidders.name
+  FROM bidders
+  JOIN bids
+    ON bidders.id = bids.bidder_id
+  GROUP BY bidders.id
+  ORDER BY bidders.id;
