ecd1c465322fb4a65d683cbf483ad18d9e6b3541
Saurabh Kamboj <skamboj97@gmail.com>
2025-04-16 23:39:17 +0530

    Modify file

--- javascript_language_fundamentals/javascript_basics/converting_a_string_to_a_number.js
+++ javascript_language_fundamentals/javascript_basics/converting_a_string_to_a_number.js
@@ -1,4 +1,16 @@
+const PAIRS = { 
+  '0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
+  '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+};
 
+function stringToInteger(string) {
+  let result = 0;
+  for (let char of string) {
+    result = (result * 10) + PAIRS[char];
+  };
 
-stringToInteger('4321');      // 4321
-stringToInteger('570');       // 570
+  return result;
+}
+
+console.log(stringToInteger('4321'));      // 4321
+console.log(stringToInteger('570'));       // 570
