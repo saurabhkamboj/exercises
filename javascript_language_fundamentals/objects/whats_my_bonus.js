88c06a535cf4d2e847af18eac2f72365ace19681
Saurabh Kamboj <skamboj97@gmail.com>
2025-05-28 13:20:01 +0530

    Add folder

---
+++ javascript_language_fundamentals/objects/whats_my_bonus.js
@@ -0,0 +1,7 @@
+function calculateBonus() {
+  return arguments[1] ? arguments[0] / 2 : 0;
+}
+
+calculateBonus(2800, true);               // 1400
+calculateBonus(1000, false);              // 0
+calculateBonus(50000, true);              // 25000
