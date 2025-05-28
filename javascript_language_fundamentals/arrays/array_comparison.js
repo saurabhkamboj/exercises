355828615baf16dcfe3f0b15d929ef0e7952bb71
Saurabh Kamboj <skamboj97@gmail.com>
2025-05-28 12:46:50 +0530

    Add file

---
+++ javascript_language_fundamentals/arrays/array_comparison.js
@@ -0,0 +1,27 @@
+function areArraysEqual(array1, array2) {
+  if (array1.length !== array2.length) return false;
+
+  let tempArray = array2.slice();
+  for (let element of array1) {
+    let index = tempArray.indexOf(element);
+    if (index === -1) {
+      return false;
+    } else {
+      tempArray.splice(index, 1);
+    }
+  }
+
+  return true;
+}
+
+console.log(areArraysEqual([1, 2, 3], [1, 2, 3]));                  // true
+console.log(areArraysEqual([1, 2, 3], [3, 2, 1]));                  // true
+console.log(areArraysEqual(['a', 'b', 'c'], ['b', 'c', 'a']));      // true
+console.log(areArraysEqual(['1', 2, 3], [1, 2, 3]));                // false
+console.log(areArraysEqual([1, 1, 2, 3], [3, 1, 2, 1]));            // true
+console.log(areArraysEqual([1, 2, 3, 4], [1, 1, 2, 3]));            // false
+console.log(areArraysEqual([1, 1, 2, 2], [4, 2, 3, 1]));            // false
+console.log(areArraysEqual([1, 1, 2], [1, 2, 2]));                  // false
+console.log(areArraysEqual([1, 1, 1], [1, 1]));                     // false
+console.log(areArraysEqual([1, 1], [1, 1]));                        // true
+console.log(areArraysEqual([1, '1'], ['1', 1]));                    // true
