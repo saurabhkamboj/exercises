9879e247cc6d4cf1b8f62a19fcadfb6a02608733
Saurabh Kamboj <skamboj97@gmail.com>
2025-05-28 17:42:06 +0530

    Add file

---
+++ javascript_small_problems/list_processing/all_substrings.js
@@ -0,0 +1,27 @@
+function substrings(string) {
+  let result = [];
+  for (let index1 = 0; index1 < string.length; index1 += 1) {
+    for (let index2 = index1; index2 < string.length; index2 += 1) {
+      result.push(string.slice(index1, index2 + 1));
+    }
+  }
+
+  return result;
+}
+
+// Further exploration
+function substrings(string) {
+  return [...string].map((_, index1) => {
+    let substring = string.slice(index1);
+    return [...substring].flatMap((_, index2) => substring.slice(0, index2 + 1))
+  })
+}
+
+console.log(substrings('abcde'));
+
+// returns
+// [ "a", "ab", "abc", "abcd", "abcde",
+//   "b", "bc", "bcd", "bcde",
+//   "c", "cd", "cde",
+//   "d", "de",
+//   "e" ]
