bc64c7eede63d07d90054430da47f4ac1b8c2bab
Saurabh Kamboj <skamboj97@gmail.com>
2025-06-02 18:04:57 +0530

    Add folder

---
+++ javascript_object_oriented_programming/function_context_scope_and_closures/the_franchise.js
@@ -0,0 +1,11 @@
+const franchise = {
+  name: 'How to Train Your Dragon',
+  allMovies() {
+    let self = this;
+    return [1, 2, 3].map(function(number) {
+      return `${self.name} ${number}`;
+    });
+  },
+};
+
+console.log(franchise.allMovies());
